d9bef0497dda5e056df567eb31da4d90459743fd
Rohit Patro <rohitpatro6@gmail.com>
2021-08-08 21:50:36 +0530

    ADDED:Test Case To Return Happy Message

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -2,7 +2,7 @@ package com.bridgelabz;
 
 public class MoodAnalyser {
     public String analyseMood(String message){
-        if (message.contains("sad")) {
+        if (message.contains("happy")) {
             return "SAD";
         } else {
             return "HAPPY";
--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -7,7 +7,7 @@ public class MoodAnalyserTest {
     @Test
     public void testMoodAnalyser() throws Exception {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("This is a sad message");
-        Assert.assertEquals(mood, "SAD");
+        String mood = moodAnalyser.analyseMood("I am in any Mood");
+        Assert.assertEquals(mood, "HAPPY");
     }
 }
